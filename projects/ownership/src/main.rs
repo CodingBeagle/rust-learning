a2c4a12fc72c0473fc76a7e0c49b4f683405235d
CodingBeagle <kasper.rie@gmail.com>
2020-01-14 23:20:58 +0100

    more learning of ownership!

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,6 +1,35 @@
 fn main() {
     string_allocation();
     string_ownership();
+    stack_ownership();
+
+    // Ownership functions
+    let hello_world = String::from("Hello, World!");
+    println!("Now I'm printing my string: {}", hello_world);
+
+    takes_ownership(hello_world);
+
+    // This line would not be valid! hello_world string was moved in function call!
+    // println!("{}", hello_world);
+
+    let my_number = 10;
+    makes_copy(my_number);
+
+    println!("My Number is: {}", my_number);
+
+    // Return value ownership
+    {
+        let my_returned_string = gives_ownership();
+        println!("It's my string! It's miine! {}", my_returned_string);
+
+        let an_awesome_string = String::from("Awesome string!");
+
+        let new_awesome_string = gives_ownership_and_takes_back(an_awesome_string);
+
+        println!("Ownership is now new_awesome_string {}", new_awesome_string);
+
+
+    }
 }
 
 fn string_allocation() {
@@ -35,4 +64,42 @@ fn string_ownership() {
 fn string_ownership_clone() {
     let s1 = String::from("hello!");
     let s2 = s1.clone();
+}
+
+fn stack_ownership() {
+    // This is allowed
+    let x = 5;
+    let y = x;
+
+    // This is because types that are stored on the stack (such as integers),
+    // can have actual copies of their data taken, meaning you get two separate values.
+    // Ruast has a special annotation called the "Copy" trait. It is used on types like
+    // Integers, or custom types that will be stored on the stack.
+    // You cannot annotate a type with the Copy trait if it also has the Drop trait.
+    // These in-built types are already "Copy" trait:
+    // - All integer types
+    // - The boolean type
+    // - All floating point types
+    // - The character type
+    // - Tuples. But ONLY if they contain types that are also "Copy".
+    println!("I can use both variables! {}, {}", x, y);
+}
+
+// For functions, passing a variable will either move or copy just like assignment.
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+} // some_string goes out of scope and "drop" is called. The underlying memory is freed.
+
+fn makes_copy(some_integer: u32) {
+    println!("{}", some_integer);
+} // some_integer goes out of scope, but nothing special happens.
+
+// For return values of functions, ownership can also be transferred.
+fn gives_ownership() -> String {
+    let hello_world = String::from("Hello, World! :D");
+    hello_world
+}
+
+fn gives_ownership_and_takes_back(some_string: String) -> String {
+    some_string
 }
